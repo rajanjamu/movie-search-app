ed9c1e288ef7c55a1ae262599a91c9ada43ce6a5
rajanjamu <rajanjamu@gmail.com>
2020-04-05 15:26:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require(request);
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let movie = `harry`;
+let url = `http://www.omdbapi.com/?s=${movie}&apikey=ff31c86d`
+
+request(url, function(error, response, body) {
+
+});
+
+app.get('/', function(req, res) {
+    res.send("This is the root URL of this application");
+});
+
+app.listen(3000, function() {
+    console.log('Express server has started!');
+});
